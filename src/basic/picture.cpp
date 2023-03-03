ef390babd7bef31fe7689712393e70dcfe5569f8
tahashieenavaz <tahashieenavaz@gmail.com>
2023-03-03 01:55:07 +0100

    nah

---
+++ src/basic/picture.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgcodecs/imgcodecs.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv) {
+	string windowName = "Output";
+	Mat image = imread(resource("img.jpg"));
+
+	errorIf(image.empty(), "Couldn't load the image");
+
+	namedWindow(windowName);
+	imshow(windowName, image);
+	waitKey(0);
+	destroyWindow(windowName);
+
+	return 0;
+}
--- src/basic/video.cpp
+++ src/basic/video.cpp
@@ -10,15 +10,11 @@ using namespace std;
 using namespace cv;
 
 int main(int argc, char** argv) {
-	string windowName = "Output";
-	Mat image = imread(resource("img.jpg"));
-
-	errorIf(image.empty(), "Couldn't load the image");
-
-	namedWindow(windowName);
-	imshow(windowName, image);
-	waitKey(0);
-	destroyWindow(windowName);
-
-	return 0;
+    VideoCapture cap(resource("video.mp4"));
+    Mat frame;
+    while(true) {
+        cap.read(frame);
+        imshow("Image", frame);
+        waitKey(1);
+    }
 }
