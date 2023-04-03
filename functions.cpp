6febeca98f1bc54365d201ed50c70c9d9ede01d0
tahashieenavaz <tahashieenavaz@gmail.com>
2023-04-03 14:01:03 +0200

    Adds comment to averageColor

--- functions.cpp
+++ functions.cpp
@@ -1,3 +1,14 @@
+/**
+ * It takes an image, a point, and a kernel size, and returns the average color of the pixels in the
+ * kernel around the point
+ * 
+ * @param image The image to get the average color from
+ * @param x x coordinate of the pixel
+ * @param y The y coordinate of the pixel.
+ * @param kernel The size of the kernel to use.
+ * 
+ * @return The average color of the image.
+ */
 cv::Scalar averageColor(cv::Mat image, int x, int y, int kernel = 9) {
 	int factor = kernel / 2;
 	int startx = max(0, x - factor);
