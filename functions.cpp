5bfbf99d864b01ae353f291c4fc61494a4b1154b
tahashieenavaz <tahashieenavaz@gmail.com>
2023-04-03 14:02:04 +0200

    Adds necessary namespaces

--- functions.cpp
+++ functions.cpp
@@ -11,10 +11,10 @@
  */
 cv::Scalar averageColor(cv::Mat image, int x, int y, int kernel = 9) {
 	int factor = kernel / 2;
-	int startx = max(0, x - factor);
-	int starty = max(0, y - factor);
-	int endx = min(image.cols - 1, x - factor);
-	int endy = min(image.rows - 1, y - factor);
-	Mat roi = image(Rect(startx, starty, endx - startx + 1, endy - starty + 1));
+	int startx = std::max(0, x - factor);
+	int starty = std::ax(0, y - factor);
+	int endx = std::min(image.cols - 1, x - factor);
+	int endy = std::min(image.rows - 1, y - factor);
+	cv::Mat roi = image(cv::Rect(startx, starty, endx - startx + 1, endy - starty + 1));
 	return cv::mean(roi);
 }
